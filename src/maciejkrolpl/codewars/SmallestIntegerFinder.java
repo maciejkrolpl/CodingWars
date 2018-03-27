abd8552caeb6c33e350f179ab20dae8b3757824c
pirat21wieka <pirat21wieka@gmail.com>
2018-03-27 22:58:57 +0200

    SmallestIntegerFinder

---
+++ src/maciejkrolpl/codewars/SmallestIntegerFinder.java
@@ -0,0 +1,13 @@
+package maciejkrolpl.codewars;
+
+public class SmallestIntegerFinder {
+    public static int findSmallestInt(int[] args) {
+        int smallest=args[0];
+
+        for (int i=1; i<args.length; i++) {
+            smallest = (smallest<args[i]) ? smallest : args[i];
+        }
+        return smallest;
+
+    }
+}
